7d6fdee203d1d712af29e8a449249f63d9a31e86
imkarimkarim <imkarimkarim@pm.me>
2023-01-22 20:12:23 +0330

    in the name of youtube

---
+++ form-with-image-preview/src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [files, setFiles] = useState();
+  const [previews, setPreviews] = useState();
+
+  // rendering previews
+  useEffect(() => {
+    if (!files) return;
+    let tmp = [];
+    for (let i = 0; i < files.length; i++) {
+      tmp.push(URL.createObjectURL(files[i]));
+    }
+    const objectUrls = tmp;
+    setPreviews(objectUrls);
+
+    // free memory
+    for (let i = 0; i < objectUrls.length; i++) {
+      return () => {
+        URL.revokeObjectURL(objectUrls[i]);
+      };
+    }
+  }, [files]);
+
+  return (
+    <main className="container">
+      <br />
+      <h3>Form with image preview</h3>
+      <input
+        type="file"
+        accept="image/jpg, image/jpeg, image/png"
+        multiple
+        onChange={(e) => {
+          if (e.target.files && e.target.files.length > 0) {
+            setFiles(e.target.files);
+          }
+        }}
+      />
+      {previews &&
+        previews.map((pic) => {
+          return <img src={pic} />;
+        })}
+    </main>
+  );
+}
+
+export default App;
